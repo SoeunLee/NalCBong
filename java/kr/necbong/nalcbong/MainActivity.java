1d9f959e8938175db1d3207bb77fb61b1a0987d8
SoeunLee <soeun2327@gmail.com>
2016-07-07 13:11:45 +0900

    플로팅 버튼 지움

--- java/kr/necbong/nalcbong/MainActivity.java
+++ java/kr/necbong/nalcbong/MainActivity.java
@@ -22,14 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().hide();       // ActionBar 숨겨줌
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
