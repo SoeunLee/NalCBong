9b09f234ddf043c86916dcad561edf390bde3e88
jaaay92 <chaejm11@naver.com>
2016-07-09 17:35:16 +0900

    알랑 저장, 취소 버튼 및 시계 추가

--- java/kr/necbong/nalcbong/AddAlarmActivity.java
+++ java/kr/necbong/nalcbong/AddAlarmActivity.java
@@ -2,6 +2,10 @@ package kr.necbong.nalcbong;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class AddAlarmActivity extends Activity {
 
@@ -9,5 +13,23 @@ public class AddAlarmActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
+        TextView saveAlarmButton = (TextView) findViewById(R.id.save_alarm);   // 버튼 : 알람 저장
+        TextView cancelAlarmButton = (TextView) findViewById(R.id.cancel_alarm); // 버튼 : 알람 취소
+        TimePicker timePicker = (TimePicker) findViewById(R.id.time_picker); // 시간 설정
+
+        saveAlarmButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        cancelAlarmButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
